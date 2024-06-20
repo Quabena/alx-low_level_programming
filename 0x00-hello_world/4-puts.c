ee4b6b092e823a453a171de4ebff2ae5c41bb344
Evans <ivanz.corbie@gmail.com>
2024-06-20 17:54:06 +0000

    4-puts file created

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+*The Program Prints exactly "Programming is like a multilingual puzzle
+*/
 int main(void)
 {
-puts("Progamming is like building a multilingual puzzle");
+puts("\"Progamming is like building a multilingual puzzle\n");
 return (0);
 }

8b66099e23b76b019f0db6c1ecab5c2f1771d196
Evans <ivanz.corbie@gmail.com>
2024-06-20 18:31:07 +0000

    5-printf file created

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-printf("%s%c", "with proper grammar, but the outcome is a piece of art,", "\n");
+printf("with proper grammar, but the outcome is a piece of art,", "\n");
 return (0);
 }

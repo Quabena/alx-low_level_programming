e01e050c1669b82a649db4ab12ba16ba58c81419
Evans <ivanz.corbie@gmail.com>
2024-06-25 20:45:53 +0000

    1-alphabet file created

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-* main - Entry point
+* print_alphabet - Prints alphabet in lowercase
 *
 * Description: Uses _putchar function to print each character of the alphabet
 *
-* Return: Always 0 (Success)
 */
-int main(void)
+void print_alphabet(void)
 {
 char letter = 'a';
 
@@ -18,5 +17,4 @@ letter++;
 }
 _putchar('\n');
 
-return (0);
 }

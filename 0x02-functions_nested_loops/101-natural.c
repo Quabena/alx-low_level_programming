d8db7766a4c7fd97fc2fd28e834cf9c39eab9f58
Evans <ivanz.corbie@gmail.com>
2024-06-26 01:13:54 +0000

    101-natural.c file created

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,45 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-* print_number - Prints an integer _putchar
-* @n: The integer to be printed
+* main - Prints the sum of all multiples of 3 or 5 up to 1024
 *
-* Description: Handles the printing of negative numbers
+* Return: Always 0 (Success)
 */
 
-void print_number(int n)
+int main(void)
 {
-unsigned int num = n;
-
-if (n < 0)
-{
-_putchar('-');
-num = -num;
-}
-
-if (num / 10)
-print_number(num / 10);
-
-_putchar((num % 10) + '0');
-}
-
-/**
-* multiple_natural_numbers - Computes and prints the sum of all the
-* multiples of 3 or 5 below 1024
-*/
-
-void multiple_natural_numbers(void)
-{
-int sum = 0;
 int i;
+int n = 0;
 
-for (i = 0; i < 1024; i++)
+while (i < 1024)
 {
-if (i % 3 == 0 || i % 5 == 0)
+if ((i % 3 == 0 || i % 5 == 0))
 {
-sum += i;
+n += i;
 }
+i++;
 }
-print_number(sum);
-_putchar('\n');
+printf("%d\n", n);
+return (0);
 }

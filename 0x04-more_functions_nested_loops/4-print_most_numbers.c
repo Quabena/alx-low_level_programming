82aff63bfd85af3b88f626cf0d81c2c4d29121dc
Evans <ivanz.corbie@gmail.com>
2024-06-28 03:47:27 +0000

    0-print_most_numbers.c file created

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_most_numbers - a function numbers from 0 yo 9
+* with the exception of 2 and 4
+*
+* Return: Always 0 (Success)
+*
+*/
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+if (i != 50)
+{
+if (i != 52)
+{
+_putchar(i);
+}
+}
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 1-isdigit.c - Is a C function that checks for a digit (0 through 9)
 2-mul.c - Is a C program that multiplies two integers
 3-print_numbers.c - Is a C function that prints the numbers from 0  to 9
+4-print_most_numbers - Is a C function that prints numbers from 0  to 9 without 2 and 4

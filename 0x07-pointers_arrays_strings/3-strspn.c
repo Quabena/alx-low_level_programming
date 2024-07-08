02a3ee65bc1e621dd646654ae3ca87eb78d14ca5
Evans <ivanz.corbie@gmail.com>
2024-07-08 14:26:29 +0000

    3-strspn.c file created

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,34 +5,39 @@
 * @s: string to search
 * @accept: string containing the characters to match
 *
-* Return: number of bytes in the initial segment of s which consist only of
-* bytes from accept
+* Return: number of bytes in the initial segment of s which consist
+* only of bytes from accept
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int count = 0;
 int match;
+char *temp_accept;
 
 while (*s != '\0')
 {
 match = 0;
-while (*accept != '\0')
+temp_accept = accept;
+
+while (*temp_accept != '\0')
 {
-if (*s == *accept)
+if (*s == *temp_accept)
 {
 count++;
 match = 1;
 break;
 }
-accept++;
+temp_accept++;
 }
+
 if (!match)
 {
 break;
 }
+
 s++;
-accept -= count;
 }
+
 return (count);
 }

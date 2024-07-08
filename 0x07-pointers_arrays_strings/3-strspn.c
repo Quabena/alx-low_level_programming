3a926cb541c2090a77b4bf7fc8e2ebf8b2f3c473
Evans <ivanz.corbie@gmail.com>
2024-07-08 14:08:33 +0000

    3-strspn.c file created

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - A function that gets the length of a prefix substring
+ * @s: string to search
+ * @accept: string containing the characters to match
+ *
+ * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+int match;
+
+while (*s != '\0')
+{
+match = 0;
+while (*accept != '\0')
+{
+if (*s == *accept)
+{
+count++;
+match = 1;
+break;
+}
+accept++;
+}
+if (!match)
+{
+break;
+}
+s++;
+accept -= count;
+}
+return count;
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 0-memset.c - Is a  function that fills memory with a constant byte.
 1-memcpy.c -Is a C function that copies memory area.
 2-strchr.c - Is a C function that locates a character in a string.
+3-strspn - Is a C function that gets the length of a prefix substring.

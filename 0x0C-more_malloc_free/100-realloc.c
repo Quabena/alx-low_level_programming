f688444b91536919fe5993ac58485a81c0e0ba96
Evans <ivanz.corbie@gmail.com>
2024-07-18 22:42:50 +0000

    100-realloc.c file created

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-* _realloc - A function that reallocates a memory block.
+* _realloc - Reallocates a memory block.
 * @ptr: Pointer to the memory previously allocated with malloc.
 * @old_size: Size of the allocated space for ptr.
 * @new_size: New size of the new memory block.
@@ -13,41 +13,42 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 char *new_ptr;
-unsigned int i, size_to_copy;
-
-if (new_size == 0)
-{
-if (ptr != NULL)
-{
-free(ptr);
-}
-return (NULL);
-}
-
-if (ptr == NULL)
-{
-return (malloc(new_size));
-}
+char *old_ptr;
+unsigned int i;
 
 if (new_size == old_size)
 {
 return (ptr);
 }
-
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+return (malloc(new_size));
+}
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 {
 return (NULL);
 }
-
-size_to_copy = (new_size < old_size) ? new_size : old_size;
-
-for (i = 0; i < size_to_copy; i++)
+old_ptr = (char *)ptr;
+if (new_size < old_size)
 {
-new_ptr[i] = ((char *)ptr)[i];
+for (i = 0; i < new_size; i++)
+{
+new_ptr[i] = old_ptr[i];
 }
-
+}
+else
+{
+for (i = 0; i < old_size; i++)
+{
+new_ptr[i] = old_ptr[i];
+}
+}
 free(ptr);
-
 return (new_ptr);
 }

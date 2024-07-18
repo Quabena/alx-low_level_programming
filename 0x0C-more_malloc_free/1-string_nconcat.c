c3cf956a4b266c69f41495e7497f4d40ad61e399
Evans <ivanz.corbie@gmail.com>
2024-07-18 21:02:42 +0000

    1-string_nconcat.c file created

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,32 +16,37 @@ char *concat;
 unsigned int i, j, len1, len2;
 
 if (s1 == NULL)
-{
 s1 = "";
-}
-
 if (s2 == NULL)
-{
 s2 = "";
+
+len1 = 0;
+while (s1[len1] != '\0')
+{
+len1++;
 }
-
-for (len1 = 0; s1[len1] != '\0';
-len1++);
-for (len2 = 0; s2[len2] != '\0';
-len2++);
-
+len2 = 0;
+while (s2[len2] != '\0')
+{
+len2++;
+}
 if (n >= len2)
+{
 n = len2;
-
+}
 concat = malloc(len1 + n + 1);
 if (concat == NULL)
+{
 return (NULL);
-
+}
 for (i = 0; i < len1; i++)
+{
 concat[i] = s1[i];
+}
 for (j = 0; j < n; j++, i++)
+{
 concat[i] = s2[j];
-
+}
 concat[i] = '\0';
 
 return (concat);

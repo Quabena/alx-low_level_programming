95254a0728e9be0c2f0e91cdc1b912c5fd771b10
Evans <ivanz.corbie@gmail.com>
2024-07-18 21:22:25 +0000

    100-realloc.c file created

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _realloc - A function that reallocates a memory block.
+* @ptr: Pointer to the memory previously allocated with malloc.
+* @old_size: Size of the allocated space for ptr.
+* @new_size: New size of the new memory block.
+* Return: Pointer to the newly allocated memory block, or NULL if new_size
+* is zero and ptr is not NULL, or if malloc fails.
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *new_ptr;
+unsigned int i, size_to_copy;
+
+if (new_size == 0)
+{
+if (ptr != NULL)
+{
+free(ptr);
+}
+return (NULL);
+}
+
+if (ptr == NULL)
+{
+return (malloc(new_size));
+}
+
+if (new_size == old_size)
+{
+return (ptr);
+}
+
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+
+size_to_copy = (new_size < old_size) ? new_size : old_size;
+for (i = 0; i < size_to_copy; i++)
+{
+new_ptr[i] = ((char *)ptr)[i];
+}
+
+free(ptr);
+
+return (new_ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -3,3 +3,4 @@
 1-string_nconcat.c - Is a C function that concatenates two strings.
 2-calloc.c - Is a C unction that allocates memory for an array, using malloc.
 3-array_range - Is a C function that creates an array of integers.
+100-realloc.c - Is a C function that reallocates a memory block using malloc and free

a762f3443c76df475a0b64169a60753c4bb8a0b4
Evans <ivanz.corbie@gmail.com>
2024-07-18 21:09:23 +0000

    2-calloc.c file created

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - A function that allocates memory for an array
+* and initializes it to zero.
+* @nmemb: Number of elements.
+* @size: Size of each element.
+* Return: Pointer to the allocated memory, initialized to zero. NULL if
+* nmemb or size is 0 or if malloc fails.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr;
+unsigned int total_size, i;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+
+total_size = nmemb * size;
+
+ptr = malloc(total_size);
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < total_size; i++)
+{
+ptr[i] = 0;
+}
+
+return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 0x0C. C - More malloc, free
 0-malloc_checked.c - Is a C function that allocates memory using malloc.
 1-string_nconcat.c - Is a C function that concatenates two strings.
+2-calloc.c - Is a C unction that allocates memory for an array, using malloc.

946ca394238c10b3ca256bf214b361871727d365
Evans <ivanz.corbie@gmail.com>
2024-07-23 01:35:56 +0000

    dog.h file created

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E. C - Structures, typedef
+dog.h - Is a C struct dog elements:name, type = char *, age, type = float, owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Structure for a dog
+* @name: Pointer to the dog's name
+* @age: The dog's age
+* @owner: Pointer to the owner's name
+* Description: This struct represents a dog with its name, age, and owner's
+* name.
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif

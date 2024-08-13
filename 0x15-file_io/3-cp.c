0022c29a4550b6cdde86098a436c324513e55fd7
Evans <ivanz.corbie@gmail.com>
2024-08-13 03:50:16 +0000

    3-cp.c file fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - A function that copies content from one file to another.
+* main - Copies content from one file to another.
 * @argc: The number of command-line arguments.
 * @argv: Array of command-line arguments.
 *
@@ -22,45 +22,27 @@ exit(97);
 
 buf = allocate_buffer(argv[2]);
 src_fd = open(argv[1], O_RDONLY);
+handle_open_error(src_fd, argv[1], 98, "Error: Can't read from file %s\n");
 
-if (src_fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buf);
-exit(98);
-}
 dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (dest_fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buf);
-close_descriptor(src_fd);
-exit(99);
-}
+handle_open_error(dest_fd, argv[2], 99, "Error: Can't write to %s\n");
+
 while ((bytes_rd = read(src_fd, buf, 1024)) > 0)
 {
 bytes_wrtn = write(dest_fd, buf, bytes_rd);
-if (bytes_wrtn != bytes_rd)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buf);
-close_descriptor(src_fd);
-close_descriptor(dest_fd);
-exit(99);
+handle_write_error(bytes_wrtn, bytes_rd, argv[2]);
 }
-}
+
 if (bytes_rd == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buf);
-close_descriptor(src_fd);
-close_descriptor(dest_fd);
 exit(98);
 }
 
 free(buf);
 close_descriptor(src_fd);
 close_descriptor(dest_fd);
+
 return (0);
 }
 
@@ -94,5 +76,39 @@ if (close(fd) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
+}
+}
+
+/**
+* handle_open_error - Handles errors related to file opening.
+* @fd: The file descriptor to check.
+* @file_name: The name of the file.
+* @error_code: The error code to exit with.
+* @error_msg: The error message to print.
+*/
+
+void handle_open_error(int fd, char *file_name, int error_code,
+		       char *error_msg)
+{
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, error_msg, file_name);
+exit(error_code);
+}
+}
+
+/**
+* handle_write_error - Handles errors related to file writing.
+* @bytes_wrtn: The number of bytes written.
+* @bytes_rd: The number of bytes read.
+* @file_name: The name of the file.
+*/
+
+void handle_write_error(ssize_t bytes_wrtn, ssize_t bytes_rd, char *file_name)
+{
+if (bytes_wrtn != bytes_rd)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
+exit(99);
 }
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,6 +15,7 @@ int _putchar(char c);
 size_t _strlen(const char *s);
 char *allocate_buffer(char *file_name);
 void close_descriptor(int fd);
-
+void handle_open_error(int fd, char *file_name, int error_code, char *error_msg);
+void handle_write_error(ssize_t bytes_wrtn, ssize_t bytes_rd, char *file_name);
 
 #endif

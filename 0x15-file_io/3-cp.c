56bbe2f2a55fb80ca5bbca4995c2d00ec3e7d2d8
Evans <ivanz.corbie@gmail.com>
2024-08-13 03:39:08 +0000

    3-cp.c file created

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,35 +1,18 @@
 #include "main.h"
 
 /**
-* main - Copies the content of a file to another file.
+* main - A function that copies content from one file to another.
 * @argc: The number of command-line arguments.
-* @argv: The array of command-line arguments.
+* @argv: Array of command-line arguments.
 *
-* Return: Always 0 on success
+* Return: 0 on success.
 */
 
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
-
-handle_arguments_and_files(argc, argv, &fd_from, &fd_to);
-copy_and_close_files(fd_from, fd_to);
-
-return (0);
-}
-
-/**
-* handle_arguments_and_files - Handles argument validation and file operations.
-* @argc: The number of command-line arguments.
-* @argv: The array of command-line arguments.
-* @fd_from: Pointer to the file descriptor for the source file.
-* @fd_to: Pointer to the file descriptor for the destination file.
-*/
-
-void handle_arguments_and_files(int argc, char *argv[], int *fd_from,
-				int *fd_to)
-{
-mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; /* rw-rw-r-- */
+int src_fd, dest_fd;
+ssize_t bytes_rd, bytes_wrtn;
+char *buf;
 
 if (argc != 3)
 {
@@ -37,64 +20,79 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
-*fd_from = open(argv[1], O_RDONLY);
-if (*fd_from == -1)
+buf = allocate_buffer(argv[2]);
+src_fd = open(argv[1], O_RDONLY);
+
+if (src_fd == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buf);
+exit(98);
+}
+dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (dest_fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buf);
+close_descriptor(src_fd);
+exit(99);
+}
+while ((bytes_rd = read(src_fd, buf, 1024)) > 0)
+{
+bytes_wrtn = write(dest_fd, buf, bytes_rd);
+if (bytes_wrtn != bytes_rd)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buf);
+close_descriptor(src_fd);
+close_descriptor(dest_fd);
+exit(99);
+}
+}
+if (bytes_rd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buf);
+close_descriptor(src_fd);
+close_descriptor(dest_fd);
 exit(98);
 }
 
-*fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-if (*fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(*fd_from);
-exit(99);
-}
+free(buf);
+close_descriptor(src_fd);
+close_descriptor(dest_fd);
+return (0);
 }
 
 /**
-* copy_and_close_files - Copies content from source to destination and
-* closes file descriptors.
-* @fd_from: The file descriptor for the source file.
-* @fd_to: The file descriptor for the destination file.
+* allocate_buffer - Allocates 1024 bytes for a buffer.
+* @file_name: The name of the file related to the buffer.
+*
+* Return: A pointer to the newly-allocated buffer.
 */
 
-void copy_and_close_files(int fd_from, int fd_to)
+char *allocate_buffer(char *file_name)
 {
-ssize_t bytes_rd, bytes_wrtn;
-char buf[BUF_SIZE];
-
-while ((bytes_rd = read(fd_from, buf, sizeof(buf))) > 0)
+char *buf;
+buf = malloc(sizeof(char) * 1024);
+if (buf == NULL)
 {
-bytes_wrtn = write(fd_to, buf, bytes_rd);
-if (bytes_wrtn != bytes_rd)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", buf);
-close(fd_from);
-close(fd_to);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
 exit(99);
 }
+return (buf);
 }
 
-if (bytes_rd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", buf);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
+/**
+* close_descriptor - Closes file descriptors.
+* @fd: The file descriptor to close.
+*/
 
-if (close(fd_from) == -1)
+void close_descriptor(int fd)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-close(fd_to);
-exit(100);
-}
-
-if (close(fd_to) == -1)
+if (close(fd) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,16 +7,14 @@
 #include <sys/stat.h>
 #include <stdio.h>
 
-#define BUF_SIZE 1024
-
-void handle_arguments_and_files(int argc, char *argv[], int *fd_from, int *fd_to);
-void copy_and_close_files(int fd_from, int fd_to);
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 
 size_t _strlen(const char *s);
+char *allocate_buffer(char *file_name);
+void close_descriptor(int fd);
+
 
 #endif

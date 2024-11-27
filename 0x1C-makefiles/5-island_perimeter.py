d918b48ee6db357e3f2c1e3f184b0edbdaa0a774
Evans <ivanz.corbie@gmail.com>
2024-11-27 21:01:06 +0000

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -56,7 +56,6 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
-                # Add the number of exposed sides of this land cell to the perimeter
                 perimeter += num_water_neighbors(grid, i, j)
 
     return perimeter

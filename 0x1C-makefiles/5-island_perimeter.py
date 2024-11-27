f367be93fdfe431ffff81415e242d4986b3628c6
Evans <ivanz.corbie@gmail.com>
2024-11-27 20:50:56 +0000

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
 
 def island_perimeter(grid):
-    """
-    Function to calculate the perimeter of the island described in the grid.
-    
-    Parameters:
-    grid (list of list of integers): A rectangular grid representing the map where
-                                      1 is land and 0 is water.
-    
-    Returns:
-    int: The perimeter of the island.
-    
-    The function iterates through each land cell and checks its neighbors to calculate
-    the perimeter of the island.
-    """
-    
+    """Returns the perimeter of the island in grid."""
+
     perimeter = 0
-    
-    # Iterate through each cell in the grid
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            # If the cell is land
-            if grid[i][j] == 1:
-                # Check four directions (left, right, up, down) to calculate perimeter
-                if i == 0 or grid[i - 1][j] == 0:  # check above
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:  # check below
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:  # check left
-                    perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:  # check right
-                    perimeter += 1
-    
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
+
     return perimeter

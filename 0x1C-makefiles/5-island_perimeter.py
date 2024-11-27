ec842d169a1631862bb486447beb9dfd0394b07b
Evans <ivanz.corbie@gmail.com>
2024-11-27 21:06:19 +0000

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
 """Module that calculates the perimeter of an island in a grid."""
 
+
 def num_water_neighbors(grid, i, j):
-    """
-    Helper function that calculates the number of water neighbors 
-    (or exposed sides) for a land cell in the grid.
-    """
-    
+    """Returns the number of water neighbors a cell has in a grid."""
+
     num = 0
 
-    # Check the cell above
     if i <= 0 or not grid[i - 1][j]:
-        num += 1  # Increment if the cell is out of bounds or is water
-
-    # Check the cell to the left
+        num += 1
     if j <= 0 or not grid[i][j - 1]:
-        num += 1  # Increment if the cell is out of bounds or is water
-
-    # Check the cell to the right
+        num += 1
     if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-        num += 1  # Increment if the cell is out of bounds or is water
-
-    # Check the cell below
+        num += 1
     if i >= len(grid) - 1 or not grid[i + 1][j]:
-        num += 1  # Increment if the cell is out of bounds or is water
+        num += 1
 
     return num
 
-def island_perimeter(grid):
-    """
-    Function to calculate the perimeter of the island described in the grid.
-    
-    The perimeter is calculated by summing the number of exposed water sides 
-    for each land cell in the grid. A land cell is considered to have an exposed 
-    side if it is adjacent to water or is on the grid boundary
-    """
-    
-    perimeter = 0
 
-    # Iterate through each cell in the grid
+def island_perimeter(grid):
+    """Returns the perimeter of the island in grid."""
+
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
